6f427e8dc3e4cd4078e90b73362454cee2bd252e
Irtaza Fiaz <irtazafayaz@gmail.com>
2022-07-17 01:31:11 +0500

    first
    first

---
+++ PrimeNumber-ComposableArchitecture/PrimeNumber_ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeNumber_ComposableArchitectureApp.swift
+//  PrimeNumber-ComposableArchitecture
+//
+//  Created by Irtaza Fiaz on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrimeNumber_ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
